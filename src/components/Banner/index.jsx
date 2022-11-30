8da42d50b86e1dd2056de5f71b6ef54629931117
Lucas Souza <108772316+SouzaLucas0@users.noreply.github.com>
2022-11-30 15:45:05 -0300

    updade

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,8 +1,10 @@
+import styles from './Banner.module.scss'
 import React from 'react'
 import bannerImg from './banner.png'
+
 const Banner = () => {
   return (
-    <div>
+    <div className={styles.banner}>
         <h1>A galeria mais completa do espaço</h1>
         <img src={bannerImg} alt="Imagem da terra vista do espaço" />
     </div>
